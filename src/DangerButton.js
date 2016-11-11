25034dcb6b987261030895081c704484ee9e4d0d
wangliang <wangliang@news.cn>
2016-11-11 14:38:53 +0800

    first commit

---
+++ src/DangerButton.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+//import Button from './Button';  //Import a component from another file
+import './Button.css';
+
+class DangerButton extends Component{
+	render(){
+		return( 
+		<div className="btn" />
+		);
+	}
+}
+
+export default DangerButton;
